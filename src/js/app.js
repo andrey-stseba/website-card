ec86d72db9988c41fc81e845dc4241273352ffd0
Andrey Stseba <scebaandru@gmail.com>
2022-03-14 14:15:50 +0200

    creating js for burger menu

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 import * as flsFunctions from './modules/functions.js';
-import Swiper, { Navigation, Pagination } from 'swiper';
+// import Swiper, { Navigation, Pagination } from 'swiper';
 
 flsFunctions.isWebp();
 
-const swiper = new Swiper();
+// const swiper = new Swiper();
